4dacea07f4f2711162a33f9b6260eb997bd2da12
linchuncheng <linchuncheng@3vjia.com>
2020-04-28 09:29:02 +0800

    [UPDATE]结构调整

---
+++ src/main/java/pet/sankei/union/controller/HealthController.java
@@ -0,0 +1,21 @@
+package pet.sankei.union.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 健康检查
+ *
+ * @author Jensen Lam
+ * @date 2020-03-20 21:06
+ **/
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String create() {
+        return "OK";
+    }
+}
