01fec04926ea01f6d0757a964d60f7376cc9c550
linchuncheng <linchuncheng@3vjia.com>
2020-04-29 11:41:31 +0800

    [UPDATE]结构调整

--- src/main/java/pet/sankei/union/controller/HealthController.java
+++ src/main/java/pet/sankei/union/controller/HealthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping
-    public String create() {
+    public String health() {
         return "OK";
     }
 }
